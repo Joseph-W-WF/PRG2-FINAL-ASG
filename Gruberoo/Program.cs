e86a4be45206aea4289157cf18aa0e56d9a4fac3
Joseph <james.101.win@gmail.com>
2026-02-05 10:49:13 +0800

    loaded 2nd feature in main program.

--- Gruberoo/Program.cs
+++ Gruberoo/Program.cs
@@ -1,1 +1,22 @@
 ﻿//Main program area
+
+List<Customer> customers =
+            FileLoader.LoadCustomers("customers.csv");
+
+List<Restaurant> restaurants = new List<Restaurant>
+        {
+            //Aydans side
+        };
+
+
+OrderLoader.LoadOrders(
+    "orders.csv",
+    customers,
+    restaurants
+);
+
+/* Simple proof (for demo/exam)
+Console.WriteLine("Customers loaded: " + customers.Count);
+Console.WriteLine("Orders for first customer: " + customers[0].Orders.Count);
+Console.WriteLine("Orders in first restaurant queue: " + restaurants[0].OrderQueue.Count);
+*/
